fb1f5070c6cff61096daa4d534c12f6db8aa48fd
handsomeleeyishan <80392816+handsomeleeyishan@users.noreply.github.com>
2021-03-11 20:59:32 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,43 @@
+input.onLogoEvent(TouchButtonEvent.LongPressed, function () {
+    basic.showString("reset")
+    敵方生命 = 5
+    我方生命 = 5
+    我方等級 = 1
+    敵方等級 = 1
+})
+input.onButtonPressed(Button.A, function () {
+    if (敵方生命 < 1) {
+        我方等級 += 1
+        我方生命 = 我方生命 * 我方等級
+        敵方生命 = 敵方生命 * 敵方等級
+        我方攻擊力 += 我方等級
+        basic.showString("P1.LV." + 我方等級)
+    } else {
+        敵方生命 += 我方攻擊力 - 我方攻擊力 - 我方攻擊力
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (我方生命 < 1) {
+        敵方等級 += 1
+        我方生命 = 我方生命 * 我方等級
+        敵方生命 = 敵方生命 * 敵方等級
+        敵方攻擊力 = 敵方等級
+        basic.showString("P2.LV" + 敵方等級)
+    } else {
+        我方生命 += 敵方攻擊力 - 敵方攻擊力 - 敵方攻擊力
+    }
+})
+let 我方攻擊力 = 0
+let 敵方攻擊力 = 0
+let 敵方等級 = 0
+let 我方等級 = 0
+let 我方生命 = 0
+let 敵方生命 = 0
+let 我方 = game.createSprite(1, 2)
+let 敵方 = game.createSprite(3, 2)
+敵方生命 = 5
+我方生命 = 5
+我方等級 = 1
+敵方等級 = 1
+敵方攻擊力 = 1
+我方攻擊力 = 1
